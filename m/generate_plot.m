c0733e9858550a1771754df3a2eb4adde65b6211
Etienne Dechamps <etienne@edechamps.fr>
2017-12-05 22:37:21 +0000

    Fix plot margins.
    For some reason, the plot margin code was accidentally commented out in
    4c2ae4f. This commit puts things back in order, fixing vertical margins
    for plots.

--- m/generate_plot.m
+++ m/generate_plot.m
@@ -6,6 +6,6 @@ function generate_plot()
 	graphics_toolkit('fltk');
 	set(gcf(), 'visible', 'off');
 	source([ 'plots/' getenv('PLOTNAME') '.m' ]);
-	%set_plot_margins();
+	set_plot_margins();
 	print(getenv('PLOTPATH'), [ '-d' getenv('PLOTTYPE') ]);
 endfunction

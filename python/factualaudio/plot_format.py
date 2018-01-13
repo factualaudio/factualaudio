4c08d89bb6b00b1b2acdec2d8e3981dfeb14053e
Etienne Dechamps <etienne@edechamps.fr>
2018-01-13 17:29:07 +0000

    Add a zero horizontal line to Python waveform plots.

--- python/factualaudio/plot_format.py
+++ python/factualaudio/plot_format.py
@@ -1,3 +1,5 @@
 def format_waveform_plot(figure):
     figure.set_figheight(2)
-    figure.axes[0].autoscale(axis='x', tight=True)
+    axes = figure.axes[0]
+    axes.autoscale(axis='x', tight=True)
+    axes.axhline(color='gray', zorder=0)

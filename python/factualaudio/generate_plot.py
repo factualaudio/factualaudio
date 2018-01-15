1d23003459838d94a9b88fe573abcfb9e7fe1d04
Etienne Dechamps <etienne@edechamps.fr>
2018-01-15 22:25:54 +0000

    Ensure the Python SVG output is deterministic.

--- python/factualaudio/generate_plot.py
+++ python/factualaudio/generate_plot.py
@@ -1,12 +1,18 @@
 import argparse
 import importlib
 from matplotlib.figure import Figure
+from matplotlib import rcParams
 
 argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument('plot_module', help='qualified plot module name')
 argument_parser.add_argument('--png-output', help='path to output PNG file')
 argument_parser.add_argument('--svg-output', help='path to output SVG file')
 args = argument_parser.parse_args()
+
+# Ensure the SVG output is deterministic. We use the module name as part
+# of the salt so that multiple SVG plots can be combined without ID
+# conflicts, in case anyone ever feels like doing that.
+rcParams['svg.hashsalt'] = 'factualaudio.com/' + args.plot_module
 
 figure = Figure()
 

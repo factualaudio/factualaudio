913153095d9deca9dc1f3e659e3ccb48a95485fc
Etienne Dechamps <etienne@edechamps.fr>
2018-01-16 21:51:57 +0000

    Migrate the sinh wave amplitude detail plot to Python.

--- python/factualaudio/plot_format.py
+++ python/factualaudio/plot_format.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def format_waveform_plot(figure):
     figure.set_figheight(2)
     axes = figure.axes[0]
@@ -8,3 +10,19 @@ def format_spectrum(figure):
     axes = figure.axes[0]
     axes.set_xscale("log")
     axes.set_xlim(20, 20000)
+
+def annotate_amplitude(axes, wave):
+    wavemin = np.amin(wave)
+    wavemax = np.amax(wave)
+    waverms = np.sqrt(np.mean(np.square(wave)))
+
+    for value in [wavemin, wavemax, waverms]:
+        axes.axhline(value, linestyle=':')
+
+    def arrow_with_text(x, y1, y2, text):
+        axes.annotate(text, xy=(x, 0), textcoords=('offset points'), xytext=(-5, 5), horizontalalignment='right')
+        axes.annotate("", xy=(x, y1), xytext=(x, y2), arrowprops=dict(arrowstyle="<->"))
+
+    arrow_with_text(1.3, wavemin, wavemax, "Peak-to-peak")
+    arrow_with_text(1.8, 0, wavemax, "Peak")
+    arrow_with_text(2.3, 0, waverms, "RMS")
--- python/factualaudio/plots/sinh_wave_amplitude_detail.py
+++ python/factualaudio/plots/sinh_wave_amplitude_detail.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def populate_figure(figure):
     num_periods = 3
-    wave = sine_wave(num_periods=num_periods)
+    wave = np.sinh(sine_wave(num_periods=num_periods) * 10) / np.sinh(10)
     axes = figure.add_subplot(1, 1, 1)
     axes.plot(np.linspace(0, num_periods, endpoint=False, num=wave.size), wave)
     format_waveform_plot(figure)

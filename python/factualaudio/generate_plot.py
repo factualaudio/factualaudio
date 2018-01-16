332bc11d1fe51a8a02bf80905b1231ed424495b1
Etienne Dechamps <etienne@edechamps.fr>
2018-01-16 21:39:09 +0000

    Add proper fonts stylesheet to Python SVG plot output.

--- python/factualaudio/generate_plot.py
+++ python/factualaudio/generate_plot.py
@@ -1,5 +1,7 @@
 import argparse
 import importlib
+import io
+import os
 from matplotlib.figure import Figure
 from matplotlib import rcParams
 
@@ -7,6 +9,7 @@ argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument('plot_module', help='qualified plot module name')
 argument_parser.add_argument('--png-output', help='path to output PNG file')
 argument_parser.add_argument('--svg-output', help='path to output SVG file')
+argument_parser.add_argument('--svg-preamble', help='insert text right after XML declaration', action='append')
 args = argument_parser.parse_args()
 
 # Ensure the SVG output is deterministic. We use the module name as part
@@ -25,5 +28,19 @@ if args.png_output is not None:
 
 if args.svg_output is not None:
     from matplotlib.backends.backend_svg import FigureCanvasSVG
-    FigureCanvasSVG(figure).print_svg(args.svg_output)
+    if args.svg_preamble is None:
+        FigureCanvasSVG(figure).print_svg(args.svg_output)
+    else:
+        svg = io.BytesIO()
+        FigureCanvasSVG(figure).print_svg(svg)
+        svg.seek(0)
+
+        svg_file = open(args.svg_output, 'wb')
+        first_line = True
+        for svg_line in svg:
+            svg_file.write(svg_line)
+            if first_line:
+                for svg_preamble in args.svg_preamble:
+                    svg_file.write((svg_preamble + os.linesep).encode())
+                first_line = False
 

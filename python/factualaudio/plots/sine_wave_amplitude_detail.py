211cfdcd066fbee3f497aa36262fa3fc793d976d
Etienne Dechamps <etienne@edechamps.fr>
2018-01-20 11:24:45 +0000

    Reduce redundancy between the amplitude detail plots.

--- python/factualaudio/plots/sine_wave_amplitude_detail.py
+++ python/factualaudio/plots/sine_wave_amplitude_detail.py
@@ -2,9 +2,9 @@ from factualaudio.data import sine_wave
 from factualaudio.plot_format import format_waveform_plot, annotate_amplitude
 import numpy as np
 
-def populate_figure(figure):
+def populate_figure(figure, wavefilter=lambda x: x):
     num_periods = 3
-    wave = sine_wave(num_periods=num_periods)
+    wave = wavefilter(sine_wave(num_periods=num_periods))
     axes = figure.add_subplot(1, 1, 1)
     axes.plot(np.linspace(0, num_periods, endpoint=False, num=wave.size), wave)
     format_waveform_plot(figure)
--- python/factualaudio/plots/sinh_wave_amplitude_detail.py
+++ python/factualaudio/plots/sinh_wave_amplitude_detail.py
@@ -1,11 +1,5 @@
-from factualaudio.data import sine_wave
-from factualaudio.plot_format import format_waveform_plot, annotate_amplitude
+import factualaudio.plots.sine_wave_amplitude_detail
 import numpy as np
 
 def populate_figure(figure):
-    num_periods = 3
-    wave = np.sinh(sine_wave(num_periods=num_periods) * 10) / np.sinh(10)
-    axes = figure.add_subplot(1, 1, 1)
-    axes.plot(np.linspace(0, num_periods, endpoint=False, num=wave.size), wave)
-    format_waveform_plot(figure)
-    annotate_amplitude(axes, wave)
+    return factualaudio.plots.sine_wave_amplitude_detail.populate_figure(figure, lambda x: np.sinh(x*10) / np.sinh(10))

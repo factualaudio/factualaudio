e0cefa70d3fe56aaf763a4fbb8b5ce3dafdff949
Etienne Dechamps <etienne@edechamps.fr>
2018-01-20 16:32:24 +0000

    Migrate the digital/analog/acoustic wave plots to Python.

---
+++ python/factualaudio/plots/sine_wave_1khz_acoustic.py
@@ -0,0 +1,10 @@
+from factualaudio.data import sine_wave
+from factualaudio.plot_format import format_waveform_plot
+from factualaudio.plot import waveform
+
+def populate_figure(figure):
+    axes = figure.add_subplot(1, 1, 1)
+    wave, sample_rate = sine_wave(frequency=1000, num_periods=2)
+    waveform(axes, wave*0.356, sample_rate)
+    format_waveform_plot(figure)
+    axes.set_ylabel('Sound pressure (Pa)')
